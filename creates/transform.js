0ee9e3928afd5ec3b1a6d9f557ac41aa677a7f42
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-05-30 15:47:45 +0530

    first commit

---
+++ creates/transform.js
@@ -0,0 +1,89 @@
+const perform = async (z, bundle) => {
+	imagetobeTransformed = "";
+	if (bundle.inputData.url.includes("https://cdn.pixelbinz0.de")) {
+		imagetobeTransformed = bundle.inputData.url;
+	} else {
+		const response = await z.request({
+			url: `https://api.pixelbinz0.de/service/platform/assets/v1.0/upload/url`,
+			method: "POST",
+			headers: {
+				accept: "application/json",
+				"Content-Type": "application/json",
+				// Add more headers as required
+			},
+			body: JSON.stringify({
+				url: bundle.inputData.url,
+				path: "/__zapier_Transfomation",
+				tags: bundle.inputData.tags,
+				// Assuming filename is static as per your example
+				access: "public-read", // Assuming access is static as per your example
+				metadata: {}, // Assuming metadata is empty as per your example
+				overwrite: true, // Use provided value or default to false
+				filenameOverride: true,
+			}),
+		});
+		imagetobeTransformed = response.data.url;
+	}
+
+	let i = bundle.inputData.foregroundType || "general";
+	let shadow = bundle.inputData.addShadow || false;
+	let r = bundle.inputData.refineOutput || true;
+	let mt = bundle.inputData.mattingType || "FineMatte";
+
+	imagetobeTransformed = imagetobeTransformed.replace(
+		"original",
+		`erase.bg(i:${i},shadow:${shadow},r:${r},mt:${mt})`
+	);
+	return { url: imagetobeTransformed };
+};
+
+module.exports = {
+	key: "transform",
+	noun: "transform",
+
+	display: {
+		label: "Remove Background",
+		description:
+			"Creates a PixeBin.io URL of the resource with removed background.",
+	},
+
+	operation: {
+		perform,
+		inputFields: [
+			{
+				key: "url",
+				required: true,
+				type: "string",
+				helpText: "URL of the image to upload.",
+			},
+			{
+				key: "foregroundType",
+				label: "Foreground Type",
+				type: "string",
+				required: false,
+				choices: ["general", "ecommerce", "car", "human"],
+			},
+			{
+				key: "addShadow",
+				label: "Add Shadow(cars only)",
+				required: false,
+				type: "boolean",
+				helpText:
+					"Adds Shadow to objects in result image (currently effective on car images only)",
+			},
+			{
+				key: "refineOutput",
+				label: "Refine Output",
+				required: false,
+				type: "boolean",
+			},
+			{
+				key: "mattingType",
+				label: "Matting Type",
+				type: "string",
+				required: false,
+				choices: { GeneralMatte: "General Matte", FineMatte: "Fine Matte" },
+			},
+		],
+	},
+};
