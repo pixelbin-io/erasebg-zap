586b0cc1314e453a8c1a81dde8166052b9ba04c1
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-06-03 14:08:12 +0530

    comments removed

--- authentication.js
+++ authentication.js
@@ -362,12 +362,7 @@ const includeApiKey = (request, z, bundle) => {
 
 module.exports = {
 	config: {
-		// "custom" is the catch-all auth type. The user supplies some info and Zapier can
-		// make authenticated requests with it
 		type: "custom",
-
-		// Define any input app's auth requires here. The user will be prompted to enter
-		// this info when they connect their account.
 		fields: [
 			{
 				key: "apiKey",
@@ -379,17 +374,8 @@ module.exports = {
 			},
 		],
 
-		// The test method allows Zapier to verify that the credentials a user provides
-		// are valid. We'll execute this method whenever a user connects their account for
-		// the first time.
 		test,
 
-		// This template string can access all the data returned from the auth test. If
-		// you return the test object, you'll access the returned data with a label like
-		// `{{json.X}}`. If you return `response.data` from your test, then your label can
-		// be `{{X}}`. This can also be a function that returns a label. That function has
-		// the standard args `(z, bundle)` and data returned from the test can be accessed
-		// in `bundle.inputData.X`.
 		connectionLabel: "{{json.app.name}} ({{json.org.name}})",
 	},
 	befores: [includeApiKey],
